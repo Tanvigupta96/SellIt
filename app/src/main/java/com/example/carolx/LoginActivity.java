fe53f5772c93048bf52391c0640d649b926282ef
Tanvigupta96 <guptatannu.34gmail.com>
2020-03-08 22:15:09 +0530

    Login part is complete

--- app/src/main/java/com/example/carolx/LoginActivity.java
+++ app/src/main/java/com/example/carolx/LoginActivity.java
@@ -1,16 +1,39 @@
 package com.example.carolx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
     private Button login_userPass;
     private Button login_gmail;
     private Button login_phone;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
+    private FirebaseAuth auth;
+    private static final String TAG = "";
+    private FirebaseAuth.AuthStateListener listener;
 
 
     @Override
@@ -18,12 +41,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        initialiseFields();
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+            finish();
+        }
+
+
+        login_userPass = findViewById(R.id.login_userPass);
+        login_gmail = findViewById(R.id.login_Google);
+        login_phone = findViewById(R.id.login_Phone);
+
+        auth = FirebaseAuth.getInstance();
+
 
         login_userPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentLogin = new Intent(LoginActivity.this,LoginUPActivity.class);
+                Intent intentLogin = new Intent(LoginActivity.this, LoginUPActivity.class);
                 startActivity(intentLogin);
 
             }
@@ -32,7 +68,7 @@ public class LoginActivity extends AppCompatActivity {
         login_gmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                signIn();
 
             }
         });
@@ -40,18 +76,103 @@ public class LoginActivity extends AppCompatActivity {
         login_phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(LoginActivity.this,CallLoginActivity.class);
+                Intent callIntent = new Intent(LoginActivity.this, CallLoginActivity.class);
                 startActivity(callIntent);
 
             }
         });
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        listener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //user is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
 
-    private void initialiseFields() {
-        login_userPass = findViewById(R.id.login_userPass);
-        login_gmail = findViewById(R.id.login_Google);
-        login_phone  = findViewById(R.id.login_Phone);
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(listener);
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (listener != null) {
+            auth.removeAuthStateListener(listener);
+        }
+    }
+
+
+    public void signIn() {
+        Intent signinIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signinIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                Log.e(TAG, "Google sign in failed");
+                // ...
+            }
+        }
 
     }
+
+
+    private void firebaseAuthWithGoogle(final GoogleSignInAccount googleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGooogle:" + googleSignInAccount.getId());
+        Log.d("firebaseAuthWithGoogle:", googleSignInAccount.getPhotoUrl() + "");
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+        auth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                    intent.putExtra("emailId", googleSignInAccount.getEmail());
+                    intent.putExtra("profilePic", googleSignInAccount.getPhotoUrl() + " ");
+                    startActivity(intent);
+                    finish();
+                } else {
+
+                    Log.w(TAG, "signInCredential:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "AUTHENTICATION ERROR", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
 }
+
+
